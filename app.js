6d55c00bb2f6e5648b308e00c244fed4f044e081
AkM-E4 <evk58364@gmail.com>
2025-04-25 03:28:30 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('video-event', (data) => {
+    socket.broadcast.emit('video-event', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
